a42d25d6a071ea1ddd1e3002311561d566b08898
ondistantshores7 <ondistantshores@optonline.net>
2025-06-20 12:59:47 -0400

    fix: respect inline font-size in buttons for preview & embed

--- public/js/player.js
+++ public/js/player.js
@@ -481,7 +481,10 @@ class IVSPlayer {
         // Apply the base styles
         // Ensure font and opacity are explicitly applied
         if (buttonStyle.fontFamily) buttonEl.style.fontFamily = buttonStyle.fontFamily;
-        if (buttonStyle.fontSize) buttonEl.style.fontSize = buttonStyle.fontSize;
+        if (buttonStyle.fontSize) {
+            buttonEl.style.fontSize = buttonStyle.fontSize;
+            buttonEl.style.lineHeight = buttonStyle.fontSize; // match height
+        }
         if (buttonStyle.opacity !== undefined) buttonEl.style.opacity = buttonStyle.opacity;
         Object.assign(buttonEl.style, buttonStyle);
 
